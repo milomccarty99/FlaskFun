e5e934b7c8dfd739814d8f2f29b49cb2c2747304
milomccarty99 <milomccarty99@github.com>
2023-01-14 16:07:02 -0800

    got it working !

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from pymongo import MongoClient
 import datetime
 app = Flask(__name__)
 
-myclient = MongoClient("mongodb:27017/")
+myclient = MongoClient("mongodb://127.0.0.1:27017/")
 mydb = myclient["mydatabase"]
 
-mycol = mydb["customers"]
+todos = mydb["todos"]
 
 print(myclient.list_database_names())
-#print(mydb.list_collection_names())
+print(mydb.list_collection_names())
 
 #todos.insertOne({'content':'blah', 'degree':'0'})
 
@@ -18,11 +18,11 @@ def index():
     if(request.method=='POST'):
         content = request.form['content']
         degree = request.form['degree']
-        #todos.insert_one({'content': content, 'degree': degree})
+        todos.insert_one({'content': content, 'degree': degree})
         return redirect(url_for('index'))
     utc_dt = datetime.datetime.utcnow()
-    #all_todos = todos.find()
-    return render_template('index.html', utc_dt=utc_dt)
+    all_todos = todos.find()
+    return render_template('index.html', utc_dt=utc_dt, blah=all_todos)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
